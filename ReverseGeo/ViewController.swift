c726318306d66307b0e424ea26524d0830f2a404
Simon Gardener <simongardener@me.com>
2018-11-07 00:06:07 +0000

    done

--- ReverseGeo/ViewController.swift
+++ ReverseGeo/ViewController.swift
@@ -7,31 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,CLLocationManagerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    @IBOutlet weak var latAndLongTextField: UITextField!
-    @IBOutlet weak var latitudeLabel: UILabel!
-    @IBOutlet weak var longitudeLabel: UILabel!
-    @IBOutlet weak var reverseGeoResult: UITextView!
     
-    @IBAction func startGeo(_ sender: UIButton) {
-        print("in start geo")
+    @IBOutlet weak var latAndLongTextField: UITextField!
+    @IBOutlet weak var reverseGeoResult: UILabel!
+    
+    @IBAction func geoFromPaste(_ sender: UIButton) {
         latAndLongTextField.resignFirstResponder()
-        guard let coordinates = extractLatAndLong() else { return }
-        print("coordinate = \(coordinates)")
+        guard let location = extractLocationfromPasteboard() else { return }
+        reverseGeoFor(location)
     }
     
-    func extractLatAndLong()-> (latitude: Double, longitude:Double)? {
+    @IBAction func startGeo(_ sender: UIButton) {
+        latAndLongTextField.resignFirstResponder()
+        guard let location = extractLocationfromTextField() else { return }
+        reverseGeoFor(location)
+        
+    }
+    func extractLocationfromPasteboard()-> CLLocation? {
+        let pasteboard = UIPasteboard.general
+        guard let locationString = pasteboard.string, locationString.isEmpty == false   else {
+            invalidInput()
+            return nil}
+        latAndLongTextField.text = locationString
+        return cllocationFrom(locationString)
+    }
+    
+    
+    func extractLocationfromTextField()-> CLLocation? {
         guard let locationString = latAndLongTextField.text, locationString.isEmpty == false else {
             invalidInput()
             return nil
         }
+        
+        return cllocationFrom(locationString)
+    }
+    
+    func cllocationFrom(_ locationString: String) -> CLLocation? {
         let spaceandtabset  = CharacterSet.init(charactersIn: " \t")
         var elements  = locationString.components(separatedBy: spaceandtabset)
         elements = elements.filter {$0 != ""}
@@ -45,12 +64,38 @@ class ViewController: UIViewController {
             invalidInput()
             return nil
         }
-        latitudeLabel.text = elements[0]
-        longitudeLabel.text = elements[1]
-        return (latitude, longitude)
+        
+        return CLLocation.init(latitude: latitude, longitude: longitude)
     }
+    func reverseGeoFor(_ location: CLLocation) {
+        let myGeocoder = CLGeocoder()
+        
+        myGeocoder.reverseGeocodeLocation(location){ (placemark, error) in
+            if error != nil {
+                print("Error, \(String(describing: error ))")
+            }
+            
+            if let place = placemark?.first {
+                let placedata  = """
+                country: \(place.country ?? "_")
+                isocode: \(place.isoCountryCode ?? "_")
+                state: \(place.administrativeArea ?? "_")
+                subadmin :\(place.subAdministrativeArea ?? "_")
+                locality :\(place.locality  ?? "_")
+                sublocality :\(place.subLocality  ?? "_")
+                bodyOfWater :\(place.ocean ?? place.inlandWater ?? "_")
+                """
+                let pasteboard = UIPasteboard.general
+                pasteboard.string = placedata
+                
+                self.reverseGeoResult.text = placedata
+            }
+        }
+    }
+    
     func invalidInput() {
         reverseGeoResult.text = "Invalid Input .\n\nInput should be 2 decimal numbers separated by a space or spaces. \n\nNo leters or symbols allowed.\n\n Latitude should be first."
+        
     }
 }
 
