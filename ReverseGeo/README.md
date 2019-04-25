718e9d74d693fb7ae8c0f0319039492ddb47dfb8
simon gardener <simongardener@me.com>
2019-04-25 19:27:42 +0100

    added ReadMe renamed methods added a range check change button text minor ui changes

---
+++ ReverseGeo/README.md
@@ -0,0 +1,11 @@
+#  Reverse Geo
+
+A simple little utility app i made for myself to perform a reverse lookup on a latitude and longitude pair.
+
+You can either type the numbers in or use whatever is on the clipboard.
+
+Only basic error error checking on text enterered, its 2 decimal numbers and they are within valid longitude and latitude ranges, either works or tells you the format that it expected.
+
+Result are copied back to clipboard/pasteboard.
+
+Uses the general pasteboard which means it should be fine with handoff. It always picks up latitude and longitude pairs I have on the mac pasteboard. I have however had issues with the results pasting going back to the mac and have had to restart my mac to make it work.
--- ReverseGeo/ViewController.swift
+++ ReverseGeo/ViewController.swift
@@ -9,62 +9,67 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController ,CLLocationManagerDelegate {
+class ViewController: UIViewController {
+
+    @IBOutlet weak var latAndLongTextField: UITextField!
+    @IBOutlet weak var reverseGeoResult: UILabel!
+    let latitudeRange = -89.9999...89.9999
+    let longitudeRange = -179.9999...179.99999
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBOutlet weak var latAndLongTextField: UITextField!
-    @IBOutlet weak var reverseGeoResult: UILabel!
-    
-    @IBAction func geoFromPaste(_ sender: UIButton) {
+    @IBAction func reverseFromClipboardTapped(_ sender: UIButton) {
         latAndLongTextField.resignFirstResponder()
         guard let location = extractLocationfromPasteboard() else { return }
         reverseGeoFor(location)
     }
     
-    @IBAction func startGeo(_ sender: UIButton) {
+    @IBAction func reverseFromTextTapped(_ sender: UIButton) {
         latAndLongTextField.resignFirstResponder()
+        startGeoFromTextField()
+    }
+    func startGeoFromTextField(){
         guard let location = extractLocationfromTextField() else { return }
         reverseGeoFor(location)
-        
     }
     func extractLocationfromPasteboard()-> CLLocation? {
         let pasteboard = UIPasteboard.general
         guard let locationString = pasteboard.string, locationString.isEmpty == false   else {
-            invalidInput()
+            displayInvalidInput()
             return nil}
         latAndLongTextField.text = locationString
-        return cllocationFrom(locationString)
+        return getLocation(from: locationString)
     }
-    
     
     func extractLocationfromTextField()-> CLLocation? {
         guard let locationString = latAndLongTextField.text, locationString.isEmpty == false else {
-            invalidInput()
+            displayInvalidInput()
             return nil
         }
         
-        return cllocationFrom(locationString)
+        return getLocation(from: locationString)
     }
     
-    func cllocationFrom(_ locationString: String) -> CLLocation? {
+    func getLocation(from locationString: String) -> CLLocation? {
         let spaceandtabset  = CharacterSet.init(charactersIn: " \t")
         var elements  = locationString.components(separatedBy: spaceandtabset)
         elements = elements.filter {$0 != ""}
         
         guard elements.count == 2 else {
-            invalidInput()
+            displayInvalidInput()
             return nil
         }
         reverseGeoResult.text = elements.joined(separator: "\n")
         guard let latitude = Double(elements[0]), let longitude = Double(elements[1]) else {
-            invalidInput()
+            displayInvalidInput()
             return nil
         }
-        
+        guard latitudeRange.contains(latitude) && longitudeRange.contains(longitude) else {
+            displayInvalidInput()
+            return nil
+        }
         return CLLocation.init(latitude: latitude, longitude: longitude)
     }
     func reverseGeoFor(_ location: CLLocation) {
@@ -86,17 +91,25 @@ class ViewController: UIViewController ,CLLocationManagerDelegate {
                 bodyOfWater :\(place.ocean ?? place.inlandWater ?? "_")
                 """
                 
-                
                 self.reverseGeoResult.text = placedata
+                
                 let pasteboard = UIPasteboard.general
-                pasteboard.string = self.reverseGeoResult.text
+                pasteboard.string = placedata
             }
         }
     }
     
-    func invalidInput() {
-        reverseGeoResult.text = "Invalid Input .\n\nInput should be 2 decimal numbers separated by a space or spaces. \n\nNo leters or symbols allowed.\n\n Latitude should be first."
-        
+    func displayInvalidInput() {
+        reverseGeoResult.text = "Invalid Input .\n\nInput should be 2 decimal numbers separated by a space or spaces. \n\nNo leters or symbols allowed.\n\n Latitude should be first.\n\n Latitude <90 >-90\n\nlongitude<180 > -180"
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        startGeoFromTextField()
+    }
+}
